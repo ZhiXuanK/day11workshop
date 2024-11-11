beae4ae5399f673a2f72bab40cc4ab677c1111b7
ZhiXuanK <kangzhixuan@u.nus.edu>
2024-11-11 15:45:45 +0800

    with comments

--- src/main/java/vttp/batch5/ssf/day11workshop/controller/ImageController.java
+++ src/main/java/vttp/batch5/ssf/day11workshop/controller/ImageController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = {"/", "/index.html"})
 public class ImageController {
     
+    //can look at chuk's code on github
+
     @GetMapping()
     public String getImage(Model model){
         File[] images = (new File("src/main/resources/static/images")).listFiles();
@@ -22,11 +24,11 @@ public class ImageController {
         //int index = rnd.nextInt(images.length);
         File file = images[rnd.nextInt(images.length)];
         //String currImg = images[rnd.nextInt(images.length)];
-        Path base = Paths.get("src/main/resources/static");
+        Path base = Paths.get("src/main/resources/static"); //
         Path img = file.toPath();
         Path relative = base.relativize(img);
         //String path = currImg.substring(currImg.indexOf("static")+6);
-        model.addAttribute("currImg", relative);
-        return "image";
+        model.addAttribute("currImg", relative); //variable/attribute within the page, we call the key in the html file to access the value
+        return "image"; //return the html file inside templates
     }
 }
